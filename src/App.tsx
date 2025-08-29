631bafa0aa4c95945ff9504833d4b305fefb68ae
BrennanPS <brenps@udel.edu>
2025-08-29 11:27:18 -0400

    Message

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Brennan Sieglen UD CISC275 with React Hooks and TypeScript
+                Brennan Sieglen Hello World UD CISC275 with React Hooks and
+                TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
